87425f693541792f431c021a541d9bf6f8f68e9c
Vorachith Kip <songta17@hotmail.com>
2022-01-04 22:12:39 +0100

    rack-core configurated / gem pry added / rspec added / factory_bot added

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,15 @@ module PharmaC
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: true,
+      controller_specs: true,
+      request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
---
+++ spec/hello_world_spec.rb
@@ -0,0 +1,5 @@
+describe "Hello World in the spec" do
+  it "spec works" do
+    expect(true).to eq true
+  end
+end
